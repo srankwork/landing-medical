1b9a853556173b3cba1da1c50ff85421f5a3c963
mukeshcoutloot <mukesh@coutloot.com>
2023-08-23 14:38:09 +0530

    deploy changes

--- src/components/ServiceIntroduction.js
+++ src/components/ServiceIntroduction.js
@@ -13,11 +13,13 @@ const ServiceIntroduction = () => {
   return (
     <section className="relative md:flex bg-appointment-btn mt-14 md:mt-28">
       <div
-        className="hidden md:block w-1/2 h-60 md:h-screen bg-cover bg-no-repeat bg-center relative"
-        style={{
-          backgroundImage: `url(https://plastic-surgeon.abelle.themerex.net/wp-content/uploads/2022/06/background-1-copyright.jpg)`,
-        }}
-      ></div>
+          className=" hidden md:block w-80 md:w-1/2 h-96 md:h-screen bg-cover bg-no-repeat bg-center"
+          style={{
+            backgroundImage: `url(https://plastic-surgeon.abelle.themerex.net/wp-content/uploads/2022/06/background-1-copyright.jpg)`,
+          }}
+        >
+        </div>
+      
       <div className="w-full md:w-1/2 p-10 text-white">
         <p className="font-semibold tracking-widest uppercase text-white">
           What we do
@@ -29,24 +31,22 @@ const ServiceIntroduction = () => {
           }}
         ></div>
         <h1 className="text-4xl md:text-5xl font-semibold leading-128 font-montserrat mt-4">
-          ADVANCED Hair Trasplant
+          Advance Hair Trasplant
         </h1>
         <p className="mt-4 md:mt-6 text-lg">
           Kurabitur suscipit nulla neque, ut laor eet erat sodales vel.
           Curabitur vitae tortor eu felis scelerisque varius a non. Curabitur et
-          finibus nunc. Aliquam erat volutpat. Sed ac pulvinar sapien. Sed
-          volutpat nunc, eu efficitur ipsum vulputate finibus.
         </p>
 
         <div className="w-full h-1 mt-12">
           <div className="w-full h-full bg-transparent animate-dashed-line" />
         </div>
 
-        <ul class="list-none p-0 md:mt-12">
+        <ul class="list-none p-0 md:mt-0">
           {serviceList.map((e) => (
             <li
               key={e}
-              class="mb-4 md:mb-8 flex items-center group hover:text-red-500 transition-transform transform hover:translate-x-2 cursor-default text-xl"
+              class="mb-4 md:mb-6 flex items-center group hover:text-red-500 transition-transform transform hover:translate-x-2 cursor-default text-xl"
             >
               <span class="pointer mr-4 inline-block w-4 h-0.5 bg-white group-hover:bg-red-500"></span>{' '}
               {e}
