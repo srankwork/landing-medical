ce12de049432b138ab4f3f46e35a148048633530
Mukesh Rajpurohit <mukeshrr1321@outlook.com>
2023-11-02 12:12:47 +0530

    thankyou page updated

--- src/pages/[name].js
+++ src/pages/[name].js
@@ -1,6 +1,6 @@
 import React from 'react';
  import { Helmet } from 'react-helmet';
- import Header from '../../components/Headers';
+ import Header from '../components/Headers';
  
  const IndexPage = () => {
    return (
